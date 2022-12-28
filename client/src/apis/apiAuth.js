a549cdf29c240b3164ad31683037459915126fa1
Nguyễn Duy Dương <81768109+duongnjas@users.noreply.github.com>
2022-12-29 00:13:48 +0700

    fix login

--- client/src/apis/apiAuth.js
+++ client/src/apis/apiAuth.js
@@ -1,11 +1,33 @@
 
 import { axiosClient } from "./axiosClient";
+const BASE_URL='http://localhost:5000/api/v1'
+const handleResponse = async (response) => {
+    if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+    }
 
+    return response.json();
+}
 const apiAuth = {
+    // postLogin: async (params) => {
+    //     const myLogin = await axiosClient.post('/auth/login', params)
+    //     return myLogin.data;
+    // },
+    
     postLogin: async (params) => {
-        const myLogin = await axiosClient.post('/auth/login', params)
-        return myLogin.data;
+        const response = await fetch(
+            `${BASE_URL}/auth/login`,
+            {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(params)
+            }
+        );
+        return handleResponse(response);
     },
+
     getUserBySocialToken: async (params) => {
         const myLogin = await axiosClient.get('/auth/social', {params})
         return myLogin.data;
