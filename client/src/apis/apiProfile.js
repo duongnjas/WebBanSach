2d58a27bb75b4ebc567b0fb38e0c07cdfdfacb17
Nguyễn Duy Dương <81768109+duongnjas@users.noreply.github.com>
2023-01-03 18:16:27 +0700

    fix

--- client/src/apis/apiProfile.js
+++ client/src/apis/apiProfile.js
@@ -54,41 +54,6 @@ const apiProfile = {
         );
         return handleResponse(response);
     },
-    ///authentication
-    // putChangePassword: async (params) => {
-    //     const res = await axiosClientWithToken.put('/user/profile/changePassword', params)
-    //     return res.data;
-    // },
-    // putUploadAvatar: async (params) => {
-    //     const res = await axiosClientWithToken.post('/user/profile/uploadAvatar', params,{headers: {
-    //         'Content-Type': 'multipart/form-data'
-    //       }})
-    //     return res.data;
-    // },
-    // putChangeInfo: async (params) => {
-    //     const res = await axiosClientWithToken.put('/user/profile/changeInfo', params)
-    //     return res.data;
-    // },
-    // getUserbyID: async (params) => {
-    //     const res = await axiosClientWithToken.get(`/user/${params}`)
-    //     return res.data;
-    // },
-    // getUserProfile: async () => {
-    //     const res = await axiosClientWithToken.get(`/user/profile`)
-    //     return res.data;
-    // },
-    // putChangePhone: async (params) => {
-    //     const res = await axiosClientWithToken.put('/user/profile/changePhone', params)
-    //     return res.data;
-    // },
-
-    // getAllUser: async (params) => {
-    //     const res = await axiosClientWithToken.get('admin/user/all', params)
-    //     return res.data;
-    // },
-
-    
-
 }
     
 export default apiProfile;
